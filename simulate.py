9255589924d5475a04788035e61ec561258a32b4
larsjensen <lgjensen5@gmail.com>
2024-01-25 18:26:51 -0500

    Left off at "Link Position"

--- simulate.py
+++ simulate.py
@@ -1,10 +1,14 @@
 import pybullet as p
 import time
+import pybullet_data
 
 # setup environment
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0) # disables debug visualizer
 
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
 
 for i in range (0, 1000):
