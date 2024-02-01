00c70a3bce292700f4196aeec2d6db466e8fef85
larsjensen <lgjensen5@gmail.com>
2024-02-01 18:24:22 -0500

    A two-link, one-joint robot. Step 28

--- generate.py
+++ generate.py
@@ -23,6 +23,7 @@ def CreateWorld():
 def CreateRobot():
     pyrosim.Start_URDF("body.urdf")
 
+    ###### TORSO #######
     # box sizes
     length = 1
     width = 1
@@ -34,6 +35,28 @@ def CreateRobot():
     z = 0.5
 
     pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length, width, height])
+
+    ###### Joint ######
+    # joint position
+    x = 0.5
+    y = 0
+    z = 1
+
+    pyrosim.Send_Joint(name = "Torso_Leg", parent= "Torso", child = "Leg", type = "revolute", position = [x,y,z])
+                                                                                                              
+    ###### Leg #######
+    # box sizes
+    length = 1
+    width = 1
+    height = 1
+
+    # box position
+    x = 1
+    y = 0
+    z = 1.5
+
+    pyrosim.Send_Cube(name="Leg", pos=[x,y,z] , size=[length, width, height])
+
     pyrosim.End()
 
 CreateWorld()
