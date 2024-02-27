fc94d63aaf7f79ec4a6fc295351829dd9be1ff2c
larsjensen <lgjensen5@gmail.com>
2024-02-27 17:56:09 -0500

    step 26 complete

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -69,8 +69,13 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
     
-    def Update_Hidden_Or_Motor_Neuron(self):
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(0)
+        for synapse in synapses:
+            if (self.Get_Name() == synapse[1]):
+                pass
+
+
 
 
 # -------------------------- Private methods -------------------------
