613e00e243269982cba30e9fda851f8b2ad1ef2a
larsjensen <lgjensen5@gmail.com>
2024-03-28 09:50:44 -0400

    step 18 complete

--- solution.py
+++ solution.py
@@ -60,9 +60,9 @@ class SOLUTION:
         height = 1
 
         # box position
-        x = 1.5
+        x = 0
         y = 0
-        z = 1.5
+        z = 1
         pyrosim.Send_Cube(name="Torso", pos=[x,y,z] , size=[length, width, height])
 
         # Joints with no upstream joint have absolute positions.
@@ -83,16 +83,21 @@ class SOLUTION:
 
         ###### Joint 1_2 ######
         # joint position
-        x = 2
-        y = 0
+        x = 0
+        y = 0.5
         z = 1
         pyrosim.Send_Joint(name = "Torso_FrontLeg", parent= "Torso", child = "FrontLeg", type = "revolute", position = [x,y,z])
                                                                                                                 
         ###### Link 2 #######
         # box position -> now is relative
-        x = 0.5
-        y = 0
-        z = -0.5
+        x = 0
+        y = 0.5
+        z = 0
+
+        length = 0.2
+        width = 1
+        height = 0.2
+        
         pyrosim.Send_Cube(name="FrontLeg", pos=[x,y,z] , size=[length, width, height])
 
         pyrosim.End()
