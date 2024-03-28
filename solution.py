846fe8bf7742458119360299775ec818962dba6d
larsjensen <lgjensen5@gmail.com>
2024-03-28 09:36:47 -0400

    step 4

--- solution.py
+++ solution.py
@@ -16,7 +16,7 @@ class SOLUTION:
         self.Create_Body()
         self.Create_Brain()
         # run
-        os.system("python3 simulate.py " + directOrGUI + " " + str(self.myID) + " &")
+        os.system("python3 simulate.py " + directOrGUI + " " + str(self.myID) + " 2&>1 &")
 
     def Wait_For_Simulation_To_End(self):
         filename = "fitness" + str(self.myID) + ".txt"
